9c325f5557c7e340f2afcaa3c6255eda11c92057
Thomas ROTSAERT <th.rotsaert@gmail.com>
2021-05-03 22:37:49 +0200

    test requete

--- app/src/main/java/com/example/applicationintervention/Login.java
+++ app/src/main/java/com/example/applicationintervention/Login.java
@@ -20,7 +20,7 @@ public class Login {
     public static Object executeQuery(String query){
         try {
 
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Class.forName("com.mysql.jc.jdbc.Driver").newInstance();
             Login.dbConnect = DriverManager.getConnection("jdbc:mysql:" + ip+"/"+db, username, password);
             dbStatement = dbConnect.createStatement();
             ResultSet rs = dbStatement.executeQuery(query);
